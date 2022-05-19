e629361fc8119d6e5b2ea235455be04bb187acdb
Naima-chokri <nematchokri@gmail.com>
2022-05-19 12:32:26 +0100

    fixing-style

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React, { Component } from "react";
 import logo from "./profile.png";
-//import "./App.css";
-import Profile from "./Profile";
-import Header from "./Header";
+import Profile from "./Components/Profile";
+
 export default class App extends Component {
   state = {
-    Person: {
-      firstName: "Naima",
-      lastName: "Chokri",
+    Info: {
+      firstName: "Naima ",
+      lastName: " Chokri",
       bio: "I’m a software engineer specializing in building (and occasionally designing) Currently, I’m focused on building web and Mobile Applications.",
       imgSrc: { logo },
       profession: "Web Developer",
       adresse: 'Happy city-Gafsa Tunis (+216) 94-902-044 ',
-      email :'naima-chokri@outlook.fr',
+      email :' naima-chokri@outlook.fr',
     },
     show: false,
-    comp:0
+    timer:0
   };
-  handleShowPerson = () => {
+  handleShowInfo = () => {
     this.setState({
-      ...this.state.Person,
+      ...this.state.Info,
       show: !this.state.show,
     });
   };
   componentDidMount = () => {
     setInterval(() => {
       this.setState((prev) => ({
-        comp: prev.comp+1,
+        timer: prev.timer+1,
       }));
     }, 1000);
   };
@@ -34,14 +33,13 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         {this.state.show && <Profile state={this.state}/>}
         <button
-          className="primary ghost"
-          onClick={this.handleShowPerson}
+          className="primary scd"
+          onClick={this.handleShowInfo}
           style={{ backgroundColor: "#231E39" }}
         >
-          Click me
+         {this.state.show?"Hide-Profile":"Sow-Profile"}
         </button>
       </div>
     );
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,23 +2,22 @@ import React, { Component } from 'react'
 
 export default class Profile extends Component {
     state={
-        comp:0
+      timer:0
     }
     componentDidMount = () => {
         setInterval(() => {
           this.setState((pre) => ({
-            comp: pre.comp+1,
+            timer: pre.timer+1,
           }));
         }, 1000);
       };
   render() {
     return (
-      <div><div>
       <div className="card-container">
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <span className="d-block d-lg-none">Clarence Taylor</span>
-            <span className="d-none d-lg-block"><img src={this.props.state.Person.imgSrc.logo} alt="user"/></span>
+            <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={this.props.state.Info.imgSrc.logo} alt="user"/></span>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
@@ -37,14 +36,18 @@ export default class Profile extends Component {
             <section className="resume-section" id="about">
                 <div className="resume-section-content">
                 <h1 className="mb-0">
-                {this.props.state.Person.firstName}
-                    <span className="text-primary">{this.props.state.Person.lastName}</span>
+                <h1 className="mb-0">
+               
+                    <span className="text-primary Sir">Sir please, give me good marks :'( </span>
+                </h1>
+                {this.props.state.Info.firstName}
+                    <span className="text-primary">{this.props.state.Info.lastName}</span>
                 </h1>
                 <div className="subheading mb-5">
-                  I'm a {this.props.state.Person.profession} based in {this.props.state.Person.adresse}
-                    <a href="mailto:naima-chori@outlook.fr">{this.props.state.Person.email}</a>
+                  I'm a {this.props.state.Info.profession} based in {this.props.state.Info.adresse}
+                    <a href="mailto:naima-chori@outlook.fr">{this.props.state.Info.email}</a>
                 </div>
-                <p className="lead mb-5">{this.props.state.Person.bio}</p>
+                <p className="lead mb-5">{this.props.state.Info.bio}</p>
                 <div className="social-icons">
                     <a className="social-icon" href="https://www.linkedin.com/in/naima-chokri-57208b11b/"><i className="fab fa-linkedin-in" /></a>
                     <a className="social-icon" href="https://github.com/Naima-chokri"><i className="fab fa-github" /></a>
@@ -52,14 +55,12 @@ export default class Profile extends Component {
                     <a className="social-icon" href="https://fr-fr.facebook.com/naima.chokri.58"><i className="fab fa-facebook-f" /></a>
                 </div>
                 </div>
-            </section>
-            <hr className="m-0" />
+            </section>    
         </div>
         <div className="buttons">
-          <button className="primary">{this.state.comp}</button>
+          <button className="primary">{this.state.timer}</button>
         </div>
       </div>
-    </div></div>
     )
   }
 }
